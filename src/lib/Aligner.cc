9be7f617a5f629c9185c1da8b269353755f1f690
odashi <yus.takara@gmail.com>
2015-06-09 20:30:28 +0900

    add NULL transition into HMM model

--- src/lib/Aligner.cc
+++ src/lib/Aligner.cc
@@ -137,6 +137,7 @@ void Aligner::trainHmmModel(
     // jumping (transition) factor
     // fj[d + distance_limit] = Fj(d) for x = [-distance_limit, distance_limit]
     vector<double> fj(2 * distance_limit + 1, 1.0);
+    double fj_null = 1.0;
 
     for (int iteration : irange(0, num_iteration)) {
         
@@ -149,6 +150,7 @@ void Aligner::trainHmmModel(
         vector<double> sumct(src_num_vocab, 0.0);
         // cj[d + distance_limit] = count(d)
         vector<double> cj(2 * distance_limit + 1, 0.0);
+        double cj_null = 0.0;
 
         double log_likelihood = 0.0;
 
@@ -169,14 +171,17 @@ void Aligner::trainHmmModel(
             // calculate jumping prob.
             // pj[is][is'] = Pj(is' -> is) = Fj(is - is') / sum[ Fj(j - is') for j = [0, src_len) ]
             vector<vector<double>> pj(src_len, vector<double>(src_len, 0.0));
+            vector<double> pj_null(src_len, 0.0);
             for (int is2 : irange(0, src_len)) {
                 double sum = 0.0;
                 for (int is : irange(is_min[is2], is_max[is2])) {
                     sum += fj[is - is2 + distance_limit];
                 }
+                sum += fj_null;
                 for (int is : irange(is_min[is2], is_max[is2])) {
                     pj[is][is2] = fj[is - is2 + distance_limit] / sum;
                 }
+                pj_null[is2] = fj_null / sum;
             }
 
             // scaling factor
@@ -185,20 +190,25 @@ void Aligner::trainHmmModel(
 
             // alpha (forward) scaled prob.
             // a[it][is]
-            vector<vector<double>> a(trg_len, vector<double>(src_len, 0.0));
+            vector<vector<double>> a(trg_len, vector<double>(2 * src_len, 0.0));
             
             // calculate alpha
             {
                 // initial
                 double sum = 0.0;
+                double initial_prob = 1.0 / (2.0 * src_len);
                 for (int is : irange(0, src_len)) {
-                    double delta = (1.0 / src_len) * pt[trg_sentence[0]][src_sentence[is]];
+                    double delta = initial_prob * pt[trg_sentence[0]][src_sentence[is]];
                     a[0][is] = delta;
+                    sum += delta;
+                    delta = initial_prob * pt[trg_sentence[0]][src_null_id];
+                    a[0][is + src_len] = delta;
                     sum += delta;
                 }
                 scale.at(0) = 1.0 / sum;
                 for (int is : irange(0, src_len)) {
                     a[0][is] *= scale[0];
+                    a[0][is + src_len] *= scale[0];
                 }
             }
             for (int it : irange(1, trg_len)) {
@@ -207,12 +217,17 @@ void Aligner::trainHmmModel(
                 for (int is : irange(0, src_len)) {
                     for (int is2 : irange(is_min[is], is_max[is])) {
                         a[it][is] += a[it - 1][is2] * pj[is][is2] * pt[trg_sentence[it]][src_sentence[is]];
+                        a[it][is] += a[it - 1][is2 + src_len] * pj[is][is2] * pt[trg_sentence[it]][src_sentence[is]];
                     }
-                    sum += a.at(it).at(is);
+                    sum += a[it][is];
+                    a[it][is + src_len] += a[it - 1][is] * pj_null[is] * pt[trg_sentence[it]][src_null_id];
+                    a[it][is + src_len] += a[it - 1][is + src_len] * pj_null[is] * pt[trg_sentence[it]][src_null_id];
+                    sum += a[it][is + src_len];
                 }
                 scale[it] = 1.0 / sum;
                 for (int is : irange(0, src_len)) {
                     a[it][is] *= scale[it];
+                    a[it][is + src_len] *= scale[it];
                 }
             }
 
@@ -223,12 +238,12 @@ void Aligner::trainHmmModel(
             
             // beta (backward) scaled prob.
             // b[it][is]
-            vector<vector<double>> b(trg_len, vector<double>(src_len, 0.0));
+            vector<vector<double>> b(trg_len, vector<double>(2 * src_len, 0.0));
 
             // calculate beta (backward)
             {
                 // final
-                for (int is : irange(0, src_len)) {
+                for (int is : irange(0, 2 * src_len)) {
                     b[trg_len - 1][is] = scale[trg_len - 1];
                 }
             }
@@ -238,13 +253,19 @@ void Aligner::trainHmmModel(
                     for (int is2 : irange(is_min[is], is_max[is])) {
                         b[it][is] += b[it + 1][is2] * pj[is2][is] * pt[trg_sentence[it + 1]][src_sentence[is2]];
                     }
-                    b.at(it).at(is) *= scale.at(it);
+                    b[it][is] += b[it + 1][is + src_len] * pj_null[is] * pt[trg_sentence[it + 1]][src_null_id];
+                    b[it][is] *= scale[it];
+                    for (int is2 : irange(0, src_len)) {
+                        b[it][is + src_len] += b[it + 1][is2] * pj[is2][is] * pt[trg_sentence[it + 1]][src_sentence[is2]];
+                    }
+                    b[it][is + src_len] += b[it + 1][is + src_len] * pj_null[is] * pt[trg_sentence[it + 1]][src_null_id];
+                    b[it][is + src_len] *= scale[it];
                 }
             }
 
             // calculate jumping counts
             // xi[is][is2] = Pr( (it-1, is2) -> (it, is) ) * sum
-            vector<vector<double>> xi(src_len, vector<double>(src_len, 0.0));
+            vector<vector<double>> xi(2 * src_len, vector<double>(2 * src_len, 0.0));
 
             for (int it : irange(1, trg_len)) {
                 double sum = 0.0;
@@ -253,20 +274,32 @@ void Aligner::trainHmmModel(
                         double delta = a[it - 1][is2] * pj[is][is2] * pt[trg_sentence[it]][src_sentence[is]] * b[it][is];
                         xi[is][is2] = delta;
                         sum += delta;
+                        delta = a[it - 1][is2 + src_len] * pj[is][is2] * pt[trg_sentence[it]][src_sentence[is]] * b[it][is];
+                        xi[is][is2 + src_len] = delta;
+                        sum += delta;
+                    }
+                    {
+                        double delta = a[it - 1][is] * pj_null[is] * pt[trg_sentence[it]][src_null_id] * b[it][is + src_len];
+                        xi[is + src_len][is] = delta;
+                        sum += delta;
+                        delta = a[it - 1][is + src_len] * pj_null[is] * pt[trg_sentence[it]][src_null_id] * b[it][is + src_len];
+                        xi[is + src_len][is + src_len] = delta;
+                        sum += delta;
                     }
                 }
-                
                 for (int is : irange(0, src_len)) {
                     for (int is2 : irange(is_min[is], is_max[is])) {
                         cj[is - is2 + distance_limit] += xi[is][is2] / sum;
-                        //cerr << (format("%2d--%2d: %2d--%2d: %.4f") % (it-1) % it % is2 % is % xi[it][is][is2]) << endl;
+                        cj[is - is2 + distance_limit] += xi[is][is2 + src_len] / sum;
                     }
+                    cj_null += xi[is + src_len][is] / sum;
+                    cj_null += xi[is + src_len][is + src_len] / sum;
                 }
             }
 
             // calculate translation counts
             // gamma[is] = Pr( (it, is) ) * sum
-            vector<double> gamma(src_len, 0.0);
+            vector<double> gamma(2 * src_len, 0.0);
 
             for (int it : irange(0, trg_len)) {
                 double sum = 0;
@@ -274,12 +307,17 @@ void Aligner::trainHmmModel(
                     double delta = a[it][is] * b[it][is];
                     gamma[is] = delta;
                     sum += delta;
+                    delta = a[it][is + src_len] * b[it][is + src_len];
+                    gamma[is + src_len] = delta;
+                    sum += delta;
                 }
                 for (int is : irange(0, src_len)) {
                     double delta = gamma[is] / sum;
                     ct[trg_sentence[it]][src_sentence[is]] += delta;
                     sumct[src_sentence[is]] += delta;
-                    //cerr << (format("%2d,%2d: %.4f") % it % is % gamma[it][is]) << endl;
+                    delta = gamma[is + src_len] / sum;
+                    ct[trg_sentence[it]][src_null_id] += delta;
+                    sumct[src_null_id] += delta;
                 }
             }
         }
@@ -290,11 +328,12 @@ void Aligner::trainHmmModel(
         // calculate pt[t][s]
         for (int t : irange(0, trg_num_vocab)) {
             for (int s : irange(0, src_num_vocab)) {
+                if (sumct[s] == 0.0) throw runtime_error("asdf!~!!");
                 pt[t][s] = ct[t][s] / sumct[s];
             }
         }
 
-        Tracer::println(2, format("LL = %.10e") % log_likelihood);
+        Tracer::println(2, format("LL = %.20e") % log_likelihood);
     }
 }
 
